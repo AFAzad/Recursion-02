93b5dae2cdaeef11fbec4bf9b161279875c33c73
afazad <abulfajle405@gmail.com>
2023-01-23 23:38:46 +0530

    Initial Commit

---
+++ ReverseStr.java
@@ -0,0 +1,17 @@
+public class ReverseStr {
+     public static void strReverse(String str, int index){
+        if(index==0){
+            System.out.println(str.charAt(index));
+            return;
+        }
+        System.out.print(str.charAt(index));
+        strReverse(str, index-1);
+     }
+
+    public static void main(String[] args){
+        String str = "a i d n i";
+        System.out.println("Original String is : "+str);
+        System.out.print("After reverse : ");
+        strReverse(str, str.length()-1);
+    }
+}
